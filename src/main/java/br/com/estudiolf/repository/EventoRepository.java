f816f23c2608dfa33bf9c870de5b3795e22eec6f
ROBSON PEREIRA DA SILVA <robson.pereira-ext@viavarejo.com.br>
2019-02-14 15:59:22 -0200

    Ajuste interno Baile para Evento

--- src/main/java/br/com/estudiolf/repository/EventoRepository.java
+++ src/main/java/br/com/estudiolf/repository/EventoRepository.java
@@ -8,7 +8,7 @@ import br.com.estudiolf.entity.Evento;
 import br.com.estudiolf.entity.Membro;
 
 public interface EventoRepository extends CrudRepository<Evento, Long> {
-	
-	@Query("SELECT count(b) FROM Baile b WHERE b.usuario = :usuario and b.dia LIKE :dia")
-	public Integer countEventos(@Param("usuario") Membro usuario, @Param("dia") String dia);
+
+    @Query("SELECT count(b) FROM Evento b WHERE b.usuario = :usuario and b.dia LIKE :dia")
+    public Integer countEventos(@Param("usuario") Membro usuario, @Param("dia") String dia);
 }
--- src/main/java/br/com/estudiolf/repository/MembroRepository.java
+++ src/main/java/br/com/estudiolf/repository/MembroRepository.java
@@ -16,9 +16,9 @@ public interface MembroRepository extends CrudRepository<Membro, Long> {
     @Query("SELECT m FROM Membro m WHERE m.nome LIKE :nome AND m.habilitado = true")
     public List<Membro> findByNomeLike(@Param("nome") String nome);
 
-    @Query("SELECT m FROM Baile b RIGHT JOIN b.usuario m with b.dia=:dia WHERE b.usuario is NULL AND m.tipo = 'ROLE_USER' AND m.habilitado=true")
+    @Query("SELECT m FROM Evento b RIGHT JOIN b.usuario m with b.dia=:dia WHERE b.usuario is NULL AND m.tipo = 'ROLE_USER' AND m.habilitado=true")
     public Iterable<Membro> findByDiaEvento(@Param("dia") String dia);
 
-    @Query("SELECT m from Baile b INNER JOIN b.usuario m WHERE b.dia=:dia AND m.tipo = 'ROLE_USER' AND m.habilitado=true")
+    @Query("SELECT m from Evento b INNER JOIN b.usuario m WHERE b.dia=:dia AND m.tipo = 'ROLE_USER' AND m.habilitado=true")
     public Iterable<Membro> findByPresenteEvento(@Param("dia") String dia);
 }
